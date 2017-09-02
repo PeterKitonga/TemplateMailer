59c3d2b3a3f73c6b5a15828f92d4d19faf3f3199
PeterKitonga <pkitonga.pk@gmail.com>
2017-09-03 01:21:41 +0300

    Added datatable js for recipients

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,6 +1,42 @@
-$(function () {
-    $(".button-collapse").sideNav();
-    $(".dropdown-button").dropdown({
-        belowOrigin: true
-    });
+$(".button-collapse").sideNav();
+$(".dropdown-button").dropdown({
+    belowOrigin: true
 });
+
+appRender = {
+    initRecipients: function() {
+        $('#recipients-table').DataTable({
+            serverSide: true,
+            ajax: '/datatables/fetch/recipients',
+            order: [[2, 'desc']],
+            dom: 'Brtip',
+            buttons: [
+                {extend: 'pageLength'},
+                {
+                    text: '<i class="icon-login"></i> Excel Import',
+                    action: function ( e, dt, node, config ) {
+                        var url = '';
+                        $("#import-excel-form").attr('action', url);
+                        $("#modal-import-excel").modal('show');
+                    }
+                },
+                {
+                    text: '<i class="icon-logout"></i> Excel Export',
+                    action: function ( e, dt, node, config ) {
+                        window.location = '';
+                    }
+                }
+            ],
+            lengthMenu: [
+                [10, 25, 50, -1],
+                ['10', '25', '50', 'Show all']
+            ],
+                columns : [
+                {data:'mail_recipient_name', name:'mail_recipient_name'},
+                {data:'mail_recipient_email', name:'mail_recipient_email'},
+                {data:'created_at', name:'created_at'},
+                {data:'actions', name:'actions', orderable: false, searchable:false}
+            ]
+        });
+    }
+};
