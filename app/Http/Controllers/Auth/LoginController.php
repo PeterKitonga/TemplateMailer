6689a849dcbe2f880bb15c28dbdf3b0583ab706a
PeterKitonga <pkitonga.pk@gmail.com>
2017-09-02 16:00:57 +0300

    Modified user authentication and registration

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\User;
+use Carbon\Carbon;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Lang;
 
 class LoginController extends Controller
 {
@@ -30,10 +35,78 @@ class LoginController extends Controller
     /**
      * Create a new controller instance.
      *
-     * @return void
      */
     public function __construct()
     {
         $this->middleware('guest', ['except' => 'logout']);
+    }
+
+    /**
+     * Get the needed authorization credentials from the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    protected function credentials(Request $request)
+    {
+        return [
+            'email' => $request->email,
+            'password' => $request->password,
+            'activation_status' => 1
+        ];
+    }
+
+    /**
+     * Send the response after the user was authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    protected function sendLoginResponse(Request $request)
+    {
+        $request->session()->regenerate();
+
+        $this->clearLoginAttempts($request);
+
+        User::query()->where('id', Auth::id())->update(['login_status' => 1]);
+
+        return $this->authenticated($request, $this->guard()->user())
+            ?: redirect()->intended($this->redirectPath());
+    }
+
+    /**
+     * Get the failed login response instance.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        $request->session()->flash('error', 'Login failed. Check your credentials and try again');
+
+        return redirect()->back()
+            ->withInput($request->only($this->username(), 'remember'))
+            ->withErrors([
+                $this->username() => Lang::get('auth.failed'),
+            ]);
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        User::query()->findOrFail(Auth::id())->update(['login_status' => 0, 'last_login' => Carbon::now()]);
+
+        $this->guard()->logout();
+
+        $request->session()->flush();
+
+        $request->session()->regenerate();
+
+        return redirect('/');
     }
 }
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\User;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
@@ -32,7 +35,6 @@ class RegisterController extends Controller
     /**
      * Create a new controller instance.
      *
-     * @return void
      */
     public function __construct()
     {
@@ -62,10 +64,91 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
-        ]);
+        $user = new User();
+        $user -> name = $data['name'];
+        $user -> email = $data['email'];
+        $user -> password = bcrypt($data['password']);
+        $user -> activation_code = str_random(60);
+        $user -> save();
+        $user -> roles()->attach(2);
+
+        return $user;
+    }
+
+    /**
+     * Show the application registration form.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function showRegistrationForm(Request $request)
+    {
+        $request->session()->flash('error', 'Registration has been disabled. Please contact the administrators for assistance');
+
+        return redirect('login');
+    }
+
+    /**
+     *  Over-ridden the register method from the "RegistersUsers" trait
+     *  Remember to take care while upgrading laravel
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function register(Request $request)
+    {
+        /*
+         * Laravel validation
+         * */
+        $validator = $this->validator($request->all());
+        if ($validator->fails())
+        {
+            $this->throwValidationException($request, $validator);
+        }
+
+        /*
+         * Using database transactions is useful here
+         * because stuff happening is actually a transaction
+         **/
+        DB::beginTransaction();
+
+        try
+        {
+            $user = $this->create($request->all());
+            /*
+             * After creating the user send an email with the random code
+             * generated in the create method above
+             **/
+            $email = new EmailVerification(new User(['activation_code' => $user->activation_code, 'name' => $user->name]));
+
+            Mail::to($user->email)->send($email);
+
+            DB::commit();
+
+            $request->session()->flash('info', 'A verification email has been sent to you. Please verify your account before proceeding');
+
+            return back();
+        }
+        catch(\Exception $e)
+        {
+            DB::rollback();
+
+            $request->session()->flash('error', 'Something went wrong. Please try again');
+
+            return back();
+        }
+    }
+
+    public function activate(Request $request, $code)
+    {
+        /**
+         * The verified method has been added to the user model
+         * and chained here for better readability
+         **/
+        User::query()->where('activation_code', $code)->firstOrFail()->activated();
+
+        $request->session()->flash('success', 'Successfully verified your account. You may proceed to login');
+
+        return redirect('login');
     }
 }
