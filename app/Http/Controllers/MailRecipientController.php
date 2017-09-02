f647d1b41283c9d0b60c2c5bc937d2a13520f2ef
PeterKitonga <pkitonga.pk@gmail.com>
2017-09-03 01:22:21 +0300

    Added some methods

--- app/Http/Controllers/MailRecipientController.php
+++ app/Http/Controllers/MailRecipientController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class MailRecipientController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        return view('recipients.index');
+    }
+
+    public function store()
+    {
+        return redirect()->back();
+    }
+
+    public function update()
+    {
+        return redirect()->back();
+    }
+
+    public function destroy()
+    {
+        return redirect()->back();
+    }
 }
