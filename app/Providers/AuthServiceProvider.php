fc1c9fd0e4230ce6de1e33ba5cd6563908548e27
PeterKitonga <pkitonga.pk@gmail.com>
2017-09-02 18:59:46 +0300

    Added some gates for permissions

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\MailTemplate;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -24,7 +25,36 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+    }
 
-        //
+    public function registerPolicies()
+    {
+        Gate::define('view-all-mail', function ($user) {
+            return $user->inRole('administrator');
+        });
+        Gate::define('create-user', function ($user) {
+            return $user->hasAccess(['create-user']);
+        });
+        Gate::define('update-user', function ($user) {
+            return $user->hasAccess(['update-user']);
+        });
+        Gate::define('deactivate-user', function ($user) {
+            return $user->hasAccess(['update-user']);
+        });
+        Gate::define('delete-user', function ($user) {
+            return $user->hasAccess(['update-user']);
+        });
+        Gate::define('create-mail', function ($user) {
+            return $user->hasAccess(['create-mail']);
+        });
+        Gate::define('update-mail', function ($user, MailTemplate $mailTemplate) {
+            return $user->hasAccess(['update-mail']) or $user->id == $mailTemplate->user_id;
+        });
+        Gate::define('schedule-mail', function ($user, MailTemplate $mailTemplate) {
+            return $user->hasAccess(['update-mail']) or $user->id == $mailTemplate->user_id;
+        });
+        Gate::define('delete-mail', function ($user, MailTemplate $mailTemplate) {
+            return $user->hasAccess(['update-mail']) or $user->id == $mailTemplate->user_id;
+        });
     }
 }
