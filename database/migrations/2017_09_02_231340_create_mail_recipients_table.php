f3115b49c839994b834a191cc840f4ea6baf37fd
PeterKitonga <pkitonga.pk@gmail.com>
2017-09-03 09:33:52 +0300

    Changed email to be unique

--- database/migrations/2017_09_02_231340_create_mail_recipients_table.php
+++ database/migrations/2017_09_02_231340_create_mail_recipients_table.php
@@ -17,7 +17,7 @@ class CreateMailRecipientsTable extends Migration
             $table->increments('id');
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->string('mail_recipient_name', 40)->nullable();
+            $table->string('mail_recipient_name', 40)->unique();
             $table->string('mail_recipient_email')->nullable();
             $table->timestamps();
             $table->softDeletes();
