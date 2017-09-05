49d8ef80c65f0a8d949fb39dbd859846ac731dcd
PeterKitonga <pkitonga.pk@gmail.com>
2017-09-05 09:29:33 +0300

    Modified mailer to accomodate dynamic mail content

--- app/Mail/SendTemplateMail.php
+++ app/Mail/SendTemplateMail.php
@@ -6,21 +6,29 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\File;
 
 class SendTemplateMail extends Mailable
 {
     use Queueable, SerializesModels;
 
+    protected $recipient;
     protected $mailData;
+    protected $filePath;
 
     /**
      * Create a new message instance.
      *
+     * @param $recipient
      * @param $mailData
+     * @param $filePath
      */
-    public function __construct($mailData)
+    public function __construct($recipient, $mailData, $filePath)
     {
+        $this->recipient = $recipient;
         $this->mailData = $mailData;
+        $this->filePath = $filePath;
     }
 
     /**
@@ -30,12 +38,28 @@ class SendTemplateMail extends Mailable
      */
     public function build()
     {
-        return $this->from(env('MAIL_FROM_ADDRESS'))
-            ->subject($this->mailData['mail_title'])
-            ->view('emails.mail')
-            ->with([
-                'title' => $this->mailData['mail_title'],
-                'mail_text' => $this->mailData['mail_body']
-            ]);
+        if ($this->mailData['mail_has_attachment'] == 1)
+        {
+            $mail = $this->from(env('MAIL_FROM_ADDRESS'))
+                ->subject($this->mailData['mail_subject'])
+                ->view('emails.mail')
+                ->with([
+                    'subject' => $this->mailData['mail_subject'],
+                    'title' => str_replace('{{name}}',$this->recipient['mail_recipient_name'], $this->mailData['mail_title']),
+                    'body' => str_replace('{{name}}',$this->recipient['mail_recipient_name'], $this->mailData['mail_body_content'])
+                ])
+                ->attach($this->filePath, ['as' => $this->mailData['mail_attachment_name'].'.pdf', 'mime' => 'application/pdf']);
+        } else {
+            $mail = $this->from(env('MAIL_FROM_ADDRESS'))
+                ->subject($this->mailData['mail_subject'])
+                ->view('emails.mail')
+                ->with([
+                    'subject' => $this->mailData['mail_subject'],
+                    'title' => str_replace('{{name}}',$this->recipient['mail_recipient_name'], $this->mailData['mail_title']),
+                    'body' => str_replace('{{name}}',$this->recipient['mail_recipient_name'], $this->mailData['mail_body_content'])
+                ]);
+        }
+
+        return $mail;
     }
 }
