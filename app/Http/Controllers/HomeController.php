f345583c9e372bfbccfe2a0ae8fc5293d5dd7955
PeterKitonga <pkitonga.pk@gmail.com>
2017-09-08 09:54:18 +0300

    Added logic for aggregates to be displayed on the home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\MailLog;
+use App\MailRecipient;
+use App\MailTemplate;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -18,10 +23,40 @@ class HomeController extends Controller
     /**
      * Show the application dashboard.
      *
+     * @param Request $request
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('home');
+        $user = $request->user();
+
+        $countLogs = MailLog::query()->where('user_id', $user->id)->where('status', '=', 'Sent')->count();
+        $countTemplates = MailTemplate::query()->where('user_id', $user->id)->count();
+        $countRecipients = MailRecipient::query()->where('user_id', $user->id)->count();
+        $graphData = self::getGraphData();
+
+        return view('home', compact('countLogs', 'countTemplates', 'countRecipients', 'graphData'));
+    }
+
+    public function getGraphData()
+    {
+        $data = MailLog::query()
+            ->select(DB::raw('COUNT(IF(status = \'Sent\', 1, NULL)) as successful'), DB::raw('COUNT(IF(status = \'Failed\', 1, NULL)) as failed'), DB::raw('DATE_FORMAT(created_at, \'%b\') as month'))
+            ->whereYear('created_at', Carbon::now()->year)
+            ->groupBy('month')
+            ->get();
+
+        if ($data)
+        {
+            $response['successful'] = array_pluck($data,'successful');
+            $response['failed'] = array_pluck($data,'failed');
+            $response['months'] = array_pluck($data,'month');
+        } else {
+            $response['successful'] = [0];
+            $response['failed'] = [0];
+            $response['months'] = [0];
+        }
+
+        return json_encode($response);
     }
 }
